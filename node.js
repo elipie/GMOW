0de9e4b711b79c1f9dc086c0bb5252fb8e241d91
Eli <72884616+elipie@users.noreply.github.com>
2020-10-28 17:02:50 -0500

    added ability to read index.html

--- node.js
+++ node.js
@@ -2,5 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html');
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
